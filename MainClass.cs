0ddd64d03dd59a97f496207969c4f0c9a693bd62
Zern <fahedalex419@gmail.com>
2026-01-04 16:54:30 -0500

    update

--- MainClass.cs
+++ MainClass.cs
@@ -1,15 +1,33 @@
-﻿using System;
+﻿using BepInEx;
+using GorillaLocomotion;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using BepInEx;
-using GorillaLocomotion;
+using Utilla.Attributes;
 namespace AntiModdedTroll
 {
+    [BepInDependency("org.legoandmars.gorillatag.utilla")]
     [BepInPlugin("Zern.NoCrash", "AntiModdedTroll", "1.0.0")]
     public class MainClass : BaseUnityPlugin
     {
-        void Awake() { GTPlayer.Instance.AddComponent<HarmonyPatches>(); }    
+        public static bool allowed;
+        void Awake() { GTPlayer.Instance.AddComponent<HarmonyPatches>(); }
+
+        //moddded stuff
+        [ModdedGamemodeJoin]
+        private void RoomJoined(string gamemode)
+        {
+
+            allowed = true;
+        }
+
+        [ModdedGamemodeLeave]
+        private void RoomLeft(string gamemode)
+        {
+
+            allowed = false;
+        }
     }
 }
