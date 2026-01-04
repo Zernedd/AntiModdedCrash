bbf53a00e6622d517087235b1fcec25fb53036c2
Zern <fahedalex419@gmail.com>
2026-01-04 16:47:44 -0500

    Upd

---
+++ HarmonyPatches.cs
@@ -0,0 +1,37 @@
+﻿using ExitGames.Client.Photon;
+using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AntiModdedTroll
+{
+    public class HarmonyPatches : MonoBehaviour
+    {
+
+        //fixes issue (come on gt fix the game please)
+        [HarmonyPatch(typeof(RoomInfo), "InternalCacheProperties")]
+        public class nocrashy
+        {
+            public static bool Prefix(RoomInfo __instance, Hashtable propertiesToCache)
+            {
+                if (__instance.masterClientId == PhotonNetwork.LocalPlayer.ActorNumber && propertiesToCache.Count == 1 && propertiesToCache.ContainsKey(248))
+                {
+                    Debug.Log("trying to crash me");
+                    return false;
+                }
+                return true;
+            }
+
+        }
+
+
+    }
+
+}
+
---
+++ MainClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using GorillaLocomotion;
+namespace AntiModdedTroll
+{
+    [BepInPlugin("Zern.NoCrash", "AntiModdedTroll", "1.0.0")]
+    public class MainClass : BaseUnityPlugin
+    {
+        void Awake() { GTPlayer.Instance.AddComponent<HarmonyPatches>(); }    
+    }
+}
